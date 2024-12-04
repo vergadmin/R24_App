7afd7d576854548eac7c60737950f3a3b6b57e62
rghosh96 <rashighosh96@gmail.com>
2024-12-04 11:05:20 -0500

    updated sponsored to recommended

--- public/scripts/generatingResults.js
+++ public/scripts/generatingResults.js
@@ -25,12 +25,12 @@ async function getResults() {
         if (ret.numTrials === 0) {
             document.getElementById("notice-area").style.display = 'none';
             button.className = "green";
-            button.innerText = "See Sponsored Study Results";
+            button.innerText = "See Recommended Study Results";
             button.disabled = false;
             button.style.backgroundColor = "#22884C"
             button.classList.add('pulse');
 
-            loadedTitle.innerText = "We were unable to find any trials with your specified criteria. But we do have sponsored trials you can observe."
+            loadedTitle.innerText = "We were unable to find any trials with your specified criteria. But we do have recommended trials you can observe."
 
             document.getElementById("still-loading").innerText = "We were unable to find any trials with your specified criteria. You can try searching again with different conditions and preferences, if you'd like.";
             
